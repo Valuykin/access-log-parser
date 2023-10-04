6588d07063dc96004f043ad90c36584059cb7bdb
DValuykin <DValuykin@inno.tech>
2023-10-04 16:38:17 +0300

    Реализованы шаги ввода 2х чисел из консоли. После ввода система вычисляет и выводит на экран Сумму, Разность, Произведение и Частное введенных чисел.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число и нажмите клавишу <Enter>:");
+        int firstNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+        System.out.println("Введите второе число и нажмите клавишу <Enter>:");
+        int secondNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+        double quotinent = (double) firstNumber / secondNumber;
+        System.out.println("Сумма введенных чисел = " + (firstNumber+secondNumber));
+        System.out.println("Разность введенных чисел = " + (firstNumber-secondNumber));
+        System.out.println("Произведение введенных чисел = " + (firstNumber*secondNumber));
+        System.out.println("Частное введенных чисел = " + quotinent);
     }
 }
