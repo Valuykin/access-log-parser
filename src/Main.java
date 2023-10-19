b908942760f35f27aa1aa7072f41444af96d115b
DValuykin <DValuykin@inno.tech>
2023-10-19 11:45:45 +0300

    реализован цикл в котором вводится путь к файлу. Система проверяет наличие файла по указанному пути. Если файл существует указывается номер корректно указанного файла. Если путь ведет в директорию или файл не существует система выдает соответствующее сообщение и продолжает работу.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        int attempt = 1;
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if(isDirectory){
+                System.out.println("Это путь к директории");
+                continue;
+            }
+            if(fileExists){
+                System.out.println("Путь корректен");
+                System.out.println("Это файл номер " + attempt);
+                attempt+=1;
+                continue;
+            }else {
+                System.out.println("Указанный файл не существует");
+            }
+        }
     }
 }
